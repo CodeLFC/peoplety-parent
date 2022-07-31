8303f7ad06c5ed4746278ee720ac08a1fb4a50a1
LFC_dorm <1240902193@qq.com>
2022-07-31 11:31:42 +0800

    添加通用工具

--- common/src/main/java/gaozhi/online/peoplety/util/StringUtil.java
+++ common/src/main/java/gaozhi/online/peoplety/util/StringUtil.java
@@ -1,4 +1,5 @@
 package gaozhi.online.peoplety.util;
+
 /**
  * @description(字符串工具)
  * @author: gaozhi.online
@@ -7,20 +8,27 @@ package gaozhi.online.peoplety.util;
  */
 public class StringUtil {
     /**
-     * @description(Returns true if, and only if, length() is 0 or if is null.)
      * @return boolean
+     * @description(Returns true if, and only if, length() is 0 or if is null.)
      * @author gaozhi.online
      * @date 2021/2/5 0005
      */
-    public static boolean isEmpty(String arg){
-        if(arg!=null){
+    public static boolean isEmpty(String arg) {
+        if (arg != null) {
             return arg.isEmpty();
         }
         return true;
     }
 
-    public static boolean equals(String arg1,String arg2){
-        if(arg1!=null){
+    public static boolean isBlank(String arg) {
+        if (arg != null) {
+            return arg.isBlank();
+        }
+        return true;
+    }
+
+    public static boolean equals(String arg1, String arg2) {
+        if (arg1 != null) {
             return arg1.equals(arg2);
         }
         return arg2 == null;
@@ -28,18 +36,19 @@ public class StringUtil {
 
     /**
      * 生成随机数
+     *
      * @param len 随机数的长度
      * @return
      */
-    public static String random(int len){
-         int min= (int) Math.pow(10,len-1);
-        int max= (int) Math.pow(10,len);
-         int random= (int) (Math.random()*max);
-         if(random<min){
-             random*=10;
-         }else if(random>=max){
-             random/=10;
-         }
-         return String.valueOf(random);
+    public static String random(int len) {
+        int min = (int) Math.pow(10, len - 1);
+        int max = (int) Math.pow(10, len);
+        int random = (int) (Math.random() * max);
+        if (random < min) {
+            random *= 10;
+        } else if (random >= max) {
+            random /= 10;
+        }
+        return String.valueOf(random);
     }
 }
