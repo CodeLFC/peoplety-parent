7ce1d85ec7f34024ff8e95f6e32655f73e92203c
LFC_dorm <1240902193@qq.com>
2022-08-05 13:34:09 +0800

    将通用远程日志部分抽象到父项目中

---
+++ common/src/main/java/gaozhi/online/peoplety/config/FeignConfiguration.java
@@ -0,0 +1,49 @@
+package gaozhi.online.peoplety.config;
+
+import feign.Contract;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * @author LiFucheng
+ * @version 1.0
+ * @description: TODO feign自带契约
+ * @date 2022/7/30 15:01
+ */
+@Slf4j
+@Configuration
+public class FeignConfiguration {
+
+    @Bean
+    public Contract feignContract() {
+        return new Contract.Default();
+    }
+
+    @Bean
+    public RequestInterceptor headerInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder
+                        .getRequestAttributes();
+                HttpServletRequest request = attributes.getRequest();
+                Enumeration<String> headerNames = request.getHeaderNames();
+                if (headerNames != null) {
+                    while (headerNames.hasMoreElements()) {
+                        String name = headerNames.nextElement();
+                        String values = request.getHeader(name);
+                        requestTemplate.header(name, values);
+                    }
+                }
+            }
+        };
+    }
+}
---
+++ common/src/main/java/gaozhi/online/peoplety/log/SysLogFeignClient.java
@@ -0,0 +1,27 @@
+package gaozhi.online.peoplety.log;
+
+import gaozhi.online.base.log.SysLog;
+import gaozhi.online.base.result.Result;
+import gaozhi.online.peoplety.config.FeignConfiguration;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @author LiFucheng
+ * @version 1.0
+ * @description: TODO 用户服务
+ * @date 2022/7/29 19:08
+ */
+@FeignClient(value = "app-peoplety-log", configuration = FeignConfiguration.class)
+public interface SysLogFeignClient {
+    /**
+    * @description:  写日志
+    * @param sysLog
+     * @return gaozhi.online.base.result.Result
+    * @author http://gaozhi.online
+    * @date: 2022/8/5 13:23
+    */
+    @PostMapping("/sys_log/post/log")
+    Result writeLog(@RequestBody SysLog sysLog);
+}
