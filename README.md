88d666f55a259484c7fd824d926656a6beff7283
LFC_dorm <1240902193@qq.com>
2022-08-05 14:16:25 +0800

    修改Feign拦截器配置，修复Content-Length与body不匹配的问题

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # peoplety-parent
-## peoplety项目的父项目
+## peoplety项目的父项目,负责公共部分：pojo、日志、异常、工具等
--- common/src/main/java/gaozhi/online/peoplety/config/FeignConfiguration.java
+++ common/src/main/java/gaozhi/online/peoplety/config/FeignConfiguration.java
@@ -4,6 +4,7 @@ import feign.Contract;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -15,14 +16,20 @@ import java.util.Enumeration;
 /**
  * @author LiFucheng
  * @version 1.0
- * @description: TODO feign自带契约
+ * @description: TODO  feign 配置
  * @date 2022/7/30 15:01
  */
 @Slf4j
 @Configuration
+@EnableFeignClients(basePackages = {"gaozhi.online.peoplety.log"})
 public class FeignConfiguration {
 
-    @Bean
+    /**
+     * @return feign.Contract
+     * @description: 废除 使用自带契约
+     * @author http://gaozhi.online
+     * @date: 2022/8/5 13:47
+     */
     public Contract feignContract() {
         return new Contract.Default();
     }
@@ -40,6 +47,10 @@ public class FeignConfiguration {
                     while (headerNames.hasMoreElements()) {
                         String name = headerNames.nextElement();
                         String values = request.getHeader(name);
+                        // 跳过 content-length body是跟Content-Length，可能导致Content-length长度跟body不一致
+                        if (name.equals("content-length")) {
+                            continue;
+                        }
                         requestTemplate.header(name, values);
                     }
                 }
