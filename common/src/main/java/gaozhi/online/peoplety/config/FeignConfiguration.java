c5ce7e91f5f580e0b268c465d60d27f54f08e877
LFC_dorm <1240902193@qq.com>
2022-09-13 16:48:49 +0800

    1.增加RestTemplate组件和Gson组件

--- common/src/main/java/gaozhi/online/peoplety/config/FeignConfiguration.java
+++ common/src/main/java/gaozhi/online/peoplety/config/FeignConfiguration.java
@@ -2,12 +2,10 @@ package gaozhi.online.peoplety.config;
 
 import feign.Contract;
 import feign.RequestInterceptor;
-import feign.RequestTemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
