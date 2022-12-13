082a9a277051289221203585bcd2d92781841c7a
LFC_dorm <1240902193@qq.com>
2022-12-13 16:54:03 +0800

    1.添加会议异常

--- common/src/main/java/gaozhi/online/peoplety/exception/enums/MeetingExceptionEnum.java
+++ common/src/main/java/gaozhi/online/peoplety/exception/enums/MeetingExceptionEnum.java
@@ -8,7 +8,7 @@ import gaozhi.online.base.result.Result;
 * @date 2022/12/13 16:50
 * @version 1.0
 */
-public enum  MeetingException implements Result.ResultEnum{
+public enum MeetingExceptionEnum implements Result.ResultEnum{
     MEETING_NOT_EXISTED(3001,"会议不存在"),
     MEETING_HAS_EXISTED(3002,"会议已存在"),
     MEETING_IS_BANED(3003,"会议被封禁")
@@ -16,7 +16,7 @@ public enum  MeetingException implements Result.ResultEnum{
     private final int  code;
     private final String message;
 
-    MeetingException(int code, String message) {
+    MeetingExceptionEnum(int code, String message) {
         this.code = code;
         this.message = message;
     }
